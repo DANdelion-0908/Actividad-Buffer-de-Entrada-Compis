1f0d039d9f69a31d9c217ae0dd66260cbbed5d90
Eunice Mata <eunice.mata2992@gmail.com>
2025-02-20 02:18:42 -0600

    procesar buffer cambiado

--- bufferSimulator.py
+++ bufferSimulator.py
@@ -1,16 +1,33 @@
-# Código base para iniciar
 def cargar_buffer(entrada, inicio, tamano_buffer):
-  buffer = entrada[inicio:inicio + tamano_buffer]
-  if len(buffer) < tamano_buffer:
-    buffer.append("eof")
-  return buffer
+    buffer = entrada[inicio:inicio + tamano_buffer]
+    if len(buffer) < tamano_buffer:
+        buffer.append("eof")
+    return buffer
 
-def procesar_buffer(buffer):
-  # Procesar y extraer lexemas del buffer
-  pass
+def procesar_buffer(entrada, tamano_buffer = 10, inicio = 0, avance = 0, lexema = ""):
+    buffer = cargar_buffer(entrada, inicio, tamano_buffer)
+    
+    while True:
+        print("                                    ", buffer)
+        while avance < len(buffer):
+            char = buffer[avance]
+            avance += 1
+            
+            if char == " " or char == "eof":
+                if lexema:
+                    print(f"Lexema procesado: {lexema}")
+                    lexema = ""
+                if char == "eof":
+                    return
+            else:
+                lexema += char
+        
+        # recargar el buffer hasta encontrar eof
+        inicio += tamano_buffer
+        avance = 0
+        buffer = cargar_buffer(entrada, inicio, tamano_buffer)
 
-entrada = list("Esto es un ejemplo eof")
-inicio = 0
-tamano_buffer = 10
-buffer = cargar_buffer(entrada, inicio, tamano_buffer)
-procesar_buffer(buffer)
+
+entrada = list("Esto es un ejemplo de entrada con eof")
+procesar_buffer(entrada)
+
